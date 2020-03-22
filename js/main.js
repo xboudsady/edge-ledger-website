d4e2483dd9368037d4291bda8d742ee98de3b709
xboudsady <boudsadyx@gmail.com>
2020-03-21 21:12:29 -0700

    Added jQuery, and enabled smooth scrolling when moving through links

--- js/main.js
+++ js/main.js
@@ -14,3 +14,17 @@ function initMap() {
     // The marker, position at location
     const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            }, 800);
+    }
+});
